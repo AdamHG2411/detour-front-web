b42f64233f3483fb7a84f9d7fe596478699f260a
Adam Grider <adam.h.grider@gmail.com>
2019-06-14 05:57:46 -0400

    add list view

--- src/Components/ListView/ListView.js
+++ src/Components/ListView/ListView.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import axios from 'axios';
 import './ListView.css';
 
-class ListView extends Component {
-	constructor(props) {
-		super(props);
-		console.log(this.props);
-	}
+function ListItem(props) {
+	return (
+		<div key={props.id} className="ListView-ListItem">
+			<h3>{props.name}</h3>
+			<p>
+				{props.lat}, {props.lng}
+			</p>
+			<p>{props.notes}</p>
+			<button onClick={axios.delete(`http://localhost:8000/detours/${props.id}`)}>Delete</button>
+		</div>
+	);
+}
 
-	render() {
+function ListView(props) {
+	if (props.map.city) {
+		let listItems = props.map.detours.map((thisDetour) => ListItem(thisDetour));
 		return (
 			<div className="ListView">
-				<p>ListView</p>
+				<h2>
+					{props.map.title} - {props.map.city}, {props.map.country}
+				</h2>
+				<p>{props.map.description}</p>
+				<button>Share (coming soon)</button>
+				<button>New Detour</button>
+				<div>{listItems}</div>
 			</div>
 		);
+	} else {
+		return <h2>Please select a map</h2>;
 	}
 }
 
